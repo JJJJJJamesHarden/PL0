44d7e63089d5b21dad3218934d616f6fa2017a36
xxcosmos <xandcosmos@gmail.com>
2019-04-26 11:34:08 +0800

    完成实验1

--- main.cpp
+++ main.cpp
@@ -9,24 +9,15 @@ int main() {
     scanf("%s", fname);
     fin = fopen(fname, "r");
     if (fin) {
-        // printf("请输入要保存的文件名\n");
-        //  scanf("%s", fname);
-        //fa1 = fopen(fname, "w");
         init();
         err = 0;
         cc = cx = ll = 0;
         ch = ' ';
-        while (ch != '.') {
-            getsym();
+        while (true) {
+            if (getsym() == -1)break;
             printf("\n");
         }
-        // fclose(fa1);
-        printf("分析完毕");
-
-    } else {
-        printf("找不到文件\n");
     }
-    // printf("\n");
     return 0;
 }
 
@@ -47,22 +38,15 @@ int getch() {
         }
         ll = 0;
         cc = 0;
-//        printf("%d", cx);
-//        fprintf(fa1, "%d ", cx);
         ch = ' ';
-        //处理一整行
+        //处理一整行 不等于换行
         while (ch != 10) {
             if (EOF == fscanf(fin, "%c", &ch)) {
                 line[ll] = 0;
                 break;
             }
-//            printf("%c", ch);
-//            fprintf(fa1, "%c", ch);
             line[ll++] = ch;
         }
-//        printf("\n");
-//        fprintf(fa1, "\n");
-
     }
     //到行末尾读下一行
     ch = line[cc++];
@@ -87,7 +71,7 @@ int getsym() {
     while (isBlank(ch)) {
         getchdo;
     }
-
+    //以字母开头的标识符
     if (isAlpha(ch)) {
         k = 0;
         do {
@@ -101,7 +85,7 @@ int getsym() {
         i = 0;
         j = norw - 1;
 
-        //搜索当前符号是否为保留字
+        //二分查找当前符号是否为保留字
         do {
             k = (i + j) / 2;
             if (strcmp(id, word[k]) <= 0) {
@@ -121,6 +105,7 @@ int getsym() {
         }
         printf("%s    %s", a, list[sym]);
     } else {
+        //数字
         if (isNumber(ch)) {
             k = 0;
             num = 0;
@@ -136,6 +121,7 @@ int getsym() {
             }
             printf("%d    %s", num, list[sym]);
         } else {
+
             if (ch == ':') {
                 getchdo;
                 if (ch == '=') {
@@ -170,96 +156,19 @@ int getsym() {
                         }
                     } else {
                         sym = ssym[ch];
-                        if (sym != period) {
+                        if (sym >= 0 && sym < 32) {
                             printf("%c   %s", ch, list[sym]);
-                            getchdo;
+                        } else {
+                            printf("错误  %c", ch);
                         }
+                        if (sym == period) {
+                            return -1;
+                        }
+                        getchdo;
                     }
                 }
             }
         }
     }
     return 0;
-}
-
-void init() {
-    int i;
-    //设置单字符符号
-    for (i = 0; i < 256; i++) {
-        ssym[i] = nul;
-    }
-    ssym['+'] = plus;
-    ssym['-'] = minus;
-    ssym['*'] = times;
-    ssym['/'] = slash;
-    ssym['('] = lparen;
-    ssym[')'] = rparen;
-    ssym['='] = eql;
-    ssym[','] = comma;
-    ssym['.'] = period;
-    ssym['#'] = neq;
-    ssym[';'] = semicolon;
-
-    strcpy(&(word[0][0]), "begin");
-    strcpy(&(word[1][0]), "call");
-    strcpy(&(word[2][0]), "const");
-    strcpy(&(word[3][0]), "do");
-    strcpy(&(word[4][0]), "end");
-    strcpy(&(word[5][0]), "if");
-    strcpy(&(word[6][0]), "odd");
-    strcpy(&(word[7][0]), "procedure");
-    strcpy(&(word[8][0]), "read");
-    strcpy(&(word[9][0]), "then");
-    strcpy(&(word[10][0]), "var");
-    strcpy(&(word[11][0]), "while");
-    strcpy(&(word[12][0]), "write");
-
-    strcpy(&(list[0][0]), "nul");
-    strcpy(&(list[1][0]), "ident");
-    strcpy(&(list[2][0]), "number");
-    strcpy(&(list[3][0]), "plus");
-    strcpy(&(list[4][0]), "minus");
-    strcpy(&(list[5][0]), "times");
-    strcpy(&(list[6][0]), "slash");
-    strcpy(&(list[7][0]), "oddsym");
-    strcpy(&(list[8][0]), "eql");
-    strcpy(&(list[9][0]), "neq");
-    strcpy(&(list[10][0]), "lss");
-    strcpy(&(list[11][0]), "leq");
-    strcpy(&(list[12][0]), "gtr");
-    strcpy(&(list[13][0]), "geq");
-    strcpy(&(list[14][0]), "lparen");
-    strcpy(&(list[15][0]), "rparen");
-    strcpy(&(list[16][0]), "comma");
-    strcpy(&(list[17][0]), "semicolon");
-    strcpy(&(list[18][0]), "period");
-    strcpy(&(list[19][0]), "becomes");
-    strcpy(&(list[20][0]), "beginsym");
-    strcpy(&(list[21][0]), "endsym");
-    strcpy(&(list[22][0]), "ifsym");
-    strcpy(&(list[23][0]), "thensym");
-    strcpy(&(list[24][0]), "whilesym");
-    strcpy(&(list[25][0]), "writesym");
-    strcpy(&(list[26][0]), "readsym");
-    strcpy(&(list[27][0]), "dosym");
-    strcpy(&(list[28][0]), "callsym");
-    strcpy(&(list[29][0]), "constsym");
-    strcpy(&(list[30][0]), "varsym");
-    strcpy(&(list[31][0]), "procsym");
-
-    wsym[0] = beginsym;
-    wsym[1] = callsym;
-    wsym[2] = constsym;
-    wsym[3] = dosym;
-    wsym[4] = endsym;
-    wsym[5] = ifsym;
-    wsym[6] = oddsym;
-    wsym[7] = procsym;
-    wsym[8] = readsym;
-    wsym[9] = thensym;
-    wsym[10] = varsym;
-    wsym[11] = whilesym;
-    wsym[12] = writesym;
-
-
 }
--- pl0.h
+++ pl0.h
@@ -10,7 +10,7 @@
 /**
  * 关键字个数
  */
-#define norw 13
+#define norw 14
 
 /**
  * number的最大位数
@@ -32,7 +32,7 @@ enum symbol {
     nul, ident, number, plus, minus, times, slash, oddsym, eql, neq,
     lss, leq, gtr, geq, lparen, rparen, comma, semicolon, period, becomes,
     beginsym, endsym, ifsym, thensym, whilesym, writesym, readsym, dosym,
-    callsym, constsym, varsym, procsym,
+    callsym, constsym, varsym, procsym, zxy
 };
 
 /**
@@ -75,7 +75,7 @@ char a[al + 1];
 /**
  * 保留字
  */
-char word[norw][al];
+char word[norw + 1][al];
 /**
  * 保留字对应的符号值
  */
@@ -101,5 +101,121 @@ int getsym();
 
 int getch();
 
-void init();
+void init() {
+    int i;
+    //设置单字符符号
+    for (i = 0; i < 256; i++) {
+        ssym[i] = nul;
+    }
+    ssym['+'] = plus;
+    ssym['-'] = minus;
+    ssym['*'] = times;
+    ssym['/'] = slash;
+    ssym['('] = lparen;
+    ssym[')'] = rparen;
+    ssym['='] = eql;
+    ssym[','] = comma;
+    ssym['.'] = period;
+    ssym['#'] = neq;
+    ssym[';'] = semicolon;
+
+    strcpy(&(word[0][0]), "begin");
+    strcpy(&(word[1][0]), "call");
+    strcpy(&(word[2][0]), "const");
+    strcpy(&(word[3][0]), "do");
+    strcpy(&(word[4][0]), "end");
+    strcpy(&(word[5][0]), "if");
+    strcpy(&(word[6][0]), "odd");
+    strcpy(&(word[7][0]), "procedure");
+    strcpy(&(word[8][0]), "read");
+    strcpy(&(word[9][0]), "then");
+    strcpy(&(word[10][0]), "var");
+    strcpy(&(word[11][0]), "while");
+    strcpy(&(word[12][0]), "write");
+    strcpy(&(word[13][0]), "zxy");
+
+    strcpy(&(list[0][0]), "nul");
+    strcpy(&(list[1][0]), "标识符");
+    strcpy(&(list[2][0]), "数字");
+    strcpy(&(list[3][0]), "运算符");
+    strcpy(&(list[4][0]), "运算符");
+    strcpy(&(list[5][0]), "运算符");
+    strcpy(&(list[6][0]), "slash");
+    strcpy(&(list[7][0]), "基本字");
+    strcpy(&(list[8][0]), "运算符");
+    strcpy(&(list[9][0]), "运算符");
+    strcpy(&(list[10][0]), "lss");
+    strcpy(&(list[11][0]), "leq");
+    strcpy(&(list[12][0]), "gtr");
+    strcpy(&(list[13][0]), "geq");
+    strcpy(&(list[14][0]), "界符");
+    strcpy(&(list[15][0]), "界符");
+    strcpy(&(list[16][0]), "界符");
+    strcpy(&(list[17][0]), "界符");
+    strcpy(&(list[18][0]), "界符");
+    strcpy(&(list[19][0]), "运算符");
+    strcpy(&(list[20][0]), "基本字");
+    strcpy(&(list[21][0]), "基本字");
+    strcpy(&(list[22][0]), "基本字");
+    strcpy(&(list[23][0]), "基本字");
+    strcpy(&(list[24][0]), "基本字");
+    strcpy(&(list[25][0]), "基本字");
+    strcpy(&(list[26][0]), "基本字");
+    strcpy(&(list[27][0]), "基本字");
+    strcpy(&(list[28][0]), "基本字");
+    strcpy(&(list[29][0]), "基本字");
+    strcpy(&(list[30][0]), "基本字");
+    strcpy(&(list[31][0]), "基本字");
+    strcpy(&(list[32][0]), "基本字");
+
+//    strcpy(&(list[0][0]), "nul");
+//    strcpy(&(list[1][0]), "ident");
+//    strcpy(&(list[2][0]), "shu");
+//    strcpy(&(list[3][0]), "plus");
+//    strcpy(&(list[4][0]), "minus");
+//    strcpy(&(list[5][0]), "times");
+//    strcpy(&(list[6][0]), "slash");
+//    strcpy(&(list[7][0]), "oddsym");
+//    strcpy(&(list[8][0]), "eql");
+//    strcpy(&(list[9][0]), "neq");
+//    strcpy(&(list[10][0]), "lss");
+//    strcpy(&(list[11][0]), "leq");
+//    strcpy(&(list[12][0]), "gtr");
+//    strcpy(&(list[13][0]), "geq");
+//    strcpy(&(list[14][0]), "lparen");
+//    strcpy(&(list[15][0]), "rparen");
+//    strcpy(&(list[16][0]), "comma");
+//    strcpy(&(list[17][0]), "semicolon");
+//    strcpy(&(list[18][0]), "period");
+//    strcpy(&(list[19][0]), "becomes");
+//    strcpy(&(list[20][0]), "beginsym");
+//    strcpy(&(list[21][0]), "endsym");
+//    strcpy(&(list[22][0]), "ifsym");
+//    strcpy(&(list[23][0]), "thensym");
+//    strcpy(&(list[24][0]), "whilesym");
+//    strcpy(&(list[25][0]), "writesym");
+//    strcpy(&(list[26][0]), "readsym");
+//    strcpy(&(list[27][0]), "dosym");
+//    strcpy(&(list[28][0]), "callsym");
+//    strcpy(&(list[29][0]), "constsym");
+//    strcpy(&(list[30][0]), "varsym");
+//    strcpy(&(list[31][0]), "procsym");
+
+    wsym[0] = beginsym;
+    wsym[1] = callsym;
+    wsym[2] = constsym;
+    wsym[3] = dosym;
+    wsym[4] = endsym;
+    wsym[5] = ifsym;
+    wsym[6] = oddsym;
+    wsym[7] = procsym;
+    wsym[8] = readsym;
+    wsym[9] = thensym;
+    wsym[10] = varsym;
+    wsym[11] = whilesym;
+    wsym[12] = writesym;
+    wsym[13] = zxy;
+
+
+}
 #endif //PL0_PL0_H
